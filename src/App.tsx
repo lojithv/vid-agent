dc450ed4848f2dab71ed90b205e5ca1ce953bc32
lojithv <lojithvinsuka1998@gmail.com>
2022-10-12 23:48:57 +0530

    basic routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
     </div>
   );
 }
 
 export default App;
+
+//old way (react router dom)
+  // eslint-disable-next-line no-lone-blocks
+  {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes> */}
